1f1fc847a5ff3ec3d6938e8bbdd89d02f955d683
Maxime Leriche <maxleriche.60@gmail.com>
2022-09-03 02:22:50 +0200

    IDK

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const month = ref("Septembre");
+const year = ref("2022");
+
+const moodType = ["Great", "Good", "MEH !", "Tense/Anxous", "Sad/Angry"];
+// https://vue-chartjs.org/guide/#creating-your-first-chart
+</script>
 
 <template>
   <div class="sheet">
@@ -9,7 +17,7 @@
       <div class="hole hidden md:block"></div>
     </div>
     <div class="title">
-      <h1>Mood & Sleep Trackers</h1>
+      <h1>Mood & Sleep Trackers : {{ month }} {{ year }}</h1>
     </div>
     <div class="content">
       <p>Content</p>

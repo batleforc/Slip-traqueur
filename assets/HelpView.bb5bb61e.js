9d46ff5ab020337cb26665af90ed19e5b6b8fcf3
batleforc <batleforc@users.noreply.github.com>
2022-09-04 04:04:18 +0000

    deploy: bdd78b8198eb7b0db16282b9c34f1769f5c63706

--- assets/HelpView.bb5bb61e.js
+++
@@ -1,8 +0,0 @@
-/**
- * name: slip-traqueur
- * version: v0.0.0
- * description: Slip traqueur
- * author: Batleforc
- * homepage: slip-traqueur.weebo.fr
- */
-import{d as _,u as p,c as n,a as d,b as e,e as s,f as r,F as u,r as c,o as a,S as m,_ as y,g as S,h as x,i as h,t as i}from"./index.5ec5ee0e.js";const k={class:"sheet"},M={class:"content"},V={class:"flex flex-col"},f=e("p",null,"Sleep",-1),g={key:0,class:"my-2 mx-6"},R=e("p",null,"Empty",-1),D=[R],C={class:"list-disc my-2 mx-6"},b=e("p",null,"Mood",-1),v={key:0,class:"my-2 mx-6"},$=e("p",null,"Empty",-1),B=[$],E={class:"list-disc my-2 mx-6"},w=_({__name:"HelpView",setup(H){const o=p();return(N,l)=>(a(),n("div",k,[d(m),d(y,{title:"Help Menu"}),e("div",M,[e("div",V,[e("button",{onClick:l[0]||(l[0]=t=>{s(S)(),s(o).ReloadData()})}," Reset Storage "),e("button",{onClick:l[1]||(l[1]=t=>{s(x)(),s(o).ReloadData()})}," Reset Mood "),e("button",{onClick:l[2]||(l[2]=t=>{s(h)(),s(o).ReloadData()})}," Reset Sleep "),e("button",{onClick:l[3]||(l[3]=t=>s(o).ReloadData())},"Reload Storage")]),e("div",null,[f,s(o).SleepValue.length==0?(a(),n("div",g,D)):r("",!0),e("ul",C,[(a(!0),n(u,null,c(s(o).SleepValue,t=>(a(),n("li",{key:t.x}," Day "+i(t.x)+" => "+i(t.y)+" : "+i(s(o).SleepHours[t.y]),1))),128))])]),e("div",null,[b,s(o).MoodValue.length==0?(a(),n("div",v,B)):r("",!0),e("ul",E,[(a(!0),n(u,null,c(s(o).MoodValue,t=>(a(),n("li",{key:t.x}," Day "+i(t.x)+" => "+i(t.y)+" : "+i(s(o).MoodType[t.y]),1))),128))])])])]))}});export{w as default};
